81d20a116bc671c3366d8fc3b86b907518d47459
Anumap-18 <bholutiwari121@gmail.com>
2022-01-02 11:29:18 +0530

    initial commit

--- app.js
+++ app.js
@@ -10,115 +10,112 @@ const list = document.querySelector("#listVal");
 const item = document.querySelector("#itemVal");
 
 //initial dynamic style
-popUpItem.style.display = 'none';
-popUpItemList.style.display = 'none';
-
+popUpItem.style.display = "none";
+popUpItemList.style.display = "none";
 
 var insertionPlace;
 
-
 // event on add button initially
 addBtn.addEventListener("click", function () {
-    if (popUpItem.style.display == 'none') {
-        popUpItem.style.display = 'block';
-    }
-    container.classList.add("active");
+  if (popUpItem.style.display == "none") {
+    popUpItem.style.display = "block";
+  }
+  container.classList.add("active");
 });
 
 // event on close button of pop up initially
 closeBtn.addEventListener("click", function () {
-    if (popUpItem.style.display == 'block') {
-        popUpItem.style.display = 'none';
-    }
-    container.classList.remove("active");
-    list.value = "";
+  if (popUpItem.style.display == "block") {
+    popUpItem.style.display = "none";
+  }
+  container.classList.remove("active");
+  list.value = "";
 });
 
 // event on add button of popup item
-document.addEventListener('click', function (e) {
-    if (e.target && e.target.id == 'addBtn') {
-        popUpItem.style.display = 'none';
-        container.classList.remove("active");
-        const listVal = list.value;
-        list.value = "";
-        if (listVal) {
-            emptyList.style.display = "none";
-        }
-        const newListItem = document.createElement('div');
-        newListItem.className = "listItem";
-        newListItem.innerHTML = `
+//creating header of each List
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "addBtn") {
+    popUpItem.style.display = "none";
+    container.classList.remove("active");
+    const listVal = list.value;
+    list.value = "";
+    if (listVal) {
+      emptyList.style.display = "none";
+    }
+
+    const newListItem = document.createElement("div");
+    newListItem.className = "listItem";
+    newListItem.innerHTML = `
         <header>${listVal}</header> 
         <ul id="listContent">
         </ul>
         <i class="fas fa-plus-circle" id="addItemBtn"></i>
         <i class="far fa-trash-alt" id="closeItemBtn"></i>
-        `
-        itemList.appendChild(newListItem);
-    }
+        `;
+    itemList.appendChild(newListItem);
+  }
 });
 
-
 // event on add button to list new item
-document.addEventListener('click', function (e) {
-    if (e.target && e.target.id == 'addItemBtn') {
-        if (popUpItemList.style.display == 'none') {
-            popUpItemList.style.display = 'block';
-        }
-        container.classList.add("active");
-        insertionPlace = e.target.parentElement.childNodes[3];
+//on click add btn in list
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "addItemBtn") {
+    if (popUpItemList.style.display == "none") {
+      popUpItemList.style.display = "block";
     }
+    container.classList.add("active");
+
+    //take ul tag from each list
+    insertionPlace = e.target.parentElement.childNodes[3];
+  }
 });
 
 // event for adding new item in the list
-document.addEventListener('click', function (e) {
-    if (e.target && e.target.id == 'addItem') {
-
-        popUpItemList.style.display = 'none';
-        container.classList.remove("active");
-        const itemVal = item.value;
-        item.value = "";
-        const li = document.createElement('li');
-        li.className = "eachItem";
-        const text = document.createTextNode(itemVal);
-        li.appendChild(text);
-        const a = document.createElement('a');
-        a.id = "markDone";
-        const linkText = document.createTextNode("mark done");
-        a.appendChild(linkText);
-        li.appendChild(a);
-        insertionPlace.appendChild(li);
-    }
+// add item in list
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "addItem") {
+    popUpItemList.style.display = "none";
+    container.classList.remove("active");
+    const itemVal = item.value;
+    item.value = "";
+    const li = document.createElement("li");
+    li.className = "eachItem";
+    const text = document.createTextNode(itemVal);
+    li.appendChild(text);
+    const a = document.createElement("a");
+    a.id = "markDone";
+    const linkText = document.createTextNode("mark done");
+    a.appendChild(linkText);
+    li.appendChild(a);
+    insertionPlace.appendChild(li);
+  }
 });
 
 // event for close button while adding new item in the list
-document.addEventListener('click', function (e) {
-    if (e.target && e.target.id == 'closeItem') {
-        popUpItemList.style.display = 'none';
-        container.classList.remove("active");
-        item.value = "";
-    }
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "closeItem") {
+    popUpItemList.style.display = "none";
+    container.classList.remove("active");
+    item.value = "";
+  }
 });
-
-
 
 // event for deleting the list
-document.addEventListener('click', function (e) {
-    if (e.target && e.target.id == 'closeItemBtn') {
-        e.target.parentElement.remove();
-        if (!itemList.getElementsByTagName("div").length) {
-            emptyList.style.display = "block";
-            console.log("xqaqd");
-        }
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "closeItemBtn") {
+    e.target.parentElement.remove();
+    if (!itemList.getElementsByTagName("div").length) {
+      emptyList.style.display = "block";
     }
+  }
 });
 
-
 // event for marking the item
-document.addEventListener('click', function (e) {
-    if (e.target && e.target.id == 'markDone') {
-        console.log(e.target.parentElement);
-        const parent = e.target.parentElement;
-        parent.classList.add("afterMark");
-        e.target.remove();
-    }
+document.addEventListener("click", function (e) {
+  if (e.target && e.target.id == "markDone") {
+    const parent = e.target.parentElement;
+    parent.classList.add("afterMark");
+    e.target.remove();
+  }
 });
